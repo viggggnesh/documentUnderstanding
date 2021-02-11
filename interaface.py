793f112f0e27441a08207d5239123501feadb9bb
Vignesh Ganesan <20452299+viggggnesh@users.noreply.github.com>
2021-02-11 14:22:33 +0530

    Created interface.py

---
+++ interaface.py
@@ -0,0 +1,12 @@
+#libraries
+import tkinter
+
+#interface code here
+top = tkinter.Tk()
+top.title('Window')
+
+def recursiveInformation():
+    tkinter.messagebox.Message('title','text')
+
+#top.mainloop()
+top.mainloop()
